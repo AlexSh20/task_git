d4b3edfcc3894954f827b4e77cf68a76fb8dea82
Alexei Shishlin <foryago@mail.ru>
2021-11-24 16:38:00 +0000

    add command 1

--- src/taskCommand.php
+++ src/taskCommand.php
@@ -4,8 +4,25 @@ namespace App;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class taskCommand extends Command
 {
-    
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:create-user';
+
+    protected function configure()
+    {
+        $this    
+           ->setName('say_hello')
+           ->setDescription('shows hello and the entered text')
+           ->addArgument('text', InputArgument::REQUIRED, 'hows hello and the entered text')
+       ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Привет '.$input->getArgument('text'));
+        return Command::SUCCESS;
+    }
 }
